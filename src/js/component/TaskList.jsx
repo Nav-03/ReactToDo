e34ca70ea873ec9151847ae8ab857c69c29b1c04
Nav-03 <navil.gabrie@gmail.com>
2022-01-12 14:43:49 +0000

    initial commit

--- src/js/component/TaskList.jsx
+++ src/js/component/TaskList.jsx
@@ -1,44 +1,56 @@
 import React, { useState } from "react";
 
 const TaskList = () => {
+	let [count, setCount] = useState(0);
 	let [task, setTask] = useState("");
 	let [list, setList] = useState([]);
+	let [inputText, setInput] = useState("");
 
-	const handleInput = (e) => {
-		if (e.keycode == 13) {
-			setTask(e.target.value);
-			setList([...list, task]);
-			setTask("");
+	const handleInput = (pressedKey) => {
+		if (pressedKey.keyCode == 32 && inputText === "") {
+			alert("Sorry, use characters!");
+			console.log("input reset!");
+			setInput("");
+		} else if (pressedKey.keyCode == 13) {
+			if (inputText === "") {
+				alert("Sorry, add task please.");
+			} else {
+				setTask(pressedKey.target.value);
+				setList([...list, task]);
+				setTask("");
+				setInput("");
+				setCount(count + 1);
+			}
 		}
 	};
 
 	return (
-		<div className="d-flex justify-content-center">
+		<div className="container-fluid">
+			<div className="pt-5 text-center mx-auto">
+				<h1 className="fw-light title opacity-25">Your Todo List</h1>
+			</div>
+
 			<div className="input-group border">
 				<input
 					className="form-control"
 					type="text"
+					value={inputText}
 					placeholder="To Do List"
-					onChange={(e) => setTask(e.target.value)}
-					onKeyDown={(e) => handleInput(e)}></input>
+					onKeyDown={(keyDown) => handleInput(keyDown)}
+					onChange={(inputKeyPress) => {
+						setTask(inputKeyPress.target.value);
+						setInput(inputKeyPress.target.value);
+					}}></input>
 			</div>
 			<div className="w-100 h-100">
 				<ul>
 					{list.map((singleTask, i) => {
-						return (
-							<li
-								className="d-flex justify-content-between ps-5 pe-4 py-2 border-end border-start border-bottom text-muted fw-light fs-5"
-								key={i}>
-								{singleTask}
-							</li>
-						);
+						return <li key={i}>{singleTask}</li>;
 					})}
+					<li className="remTasks">
+						You have {count} tasks remaining!
+					</li>
 				</ul>
-			</div>
-			<div className="w-100">
-				<div className="ps-5 py-1 border-end border-start border-bottom text-muted fw-light fs-6 text-start">
-					<span>{list.length} Items</span>
-				</div>
 			</div>
 		</div>
 	);
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import TaskList from "./TaskList.jsx";
 
 //include images into your bundle
@@ -6,16 +6,9 @@ import TaskList from "./TaskList.jsx";
 //create your first component
 const Home = () => {
 	return (
-		<div className="container-fluid">
-			<div className="pt-5 text-center mx-auto">
-				<h1 className="fw-light title opacity-25">todos</h1>
-			</div>
-			<div className="row px-5 d-flex justify-content-center">
-				<div className="text-center d-flex justify-content-center">
-					<TaskList />
-				</div>
-			</div>
-		</div>
+		<>
+			<TaskList />
+		</>
 	);
 };
 
